67a5342d8c3f4ddfa98d21ec5e3db4e24387d324
MaYe <maye634028187@icloud.com>
2014-03-15 12:10:43 +0800

    Initial Commit

---
+++ Rucksack/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rucksack
+//
+//  Created by MaYe on 14-3-15.
+//  Copyright (c) 2014年 MaYe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rucksack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rucksack
+//
+//  Created by MaYe on 14-3-15.
+//  Copyright (c) 2014年 MaYe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
